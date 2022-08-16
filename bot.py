ce415931d3ef5128cf48078cd8cf4e6fcc9290f7
mer1us23 <88798519+mer1us23@users.noreply.github.com>
2022-08-16 19:38:50 +0300

    bot

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 # CONNECT DISCORD BOT, SET PREFIX COMMAND AND BOT STATUS
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='bro ', intents=intents)
+bot = commands.Bot(command_prefix=' ', intents=intents)
 
 @bot.event
 async def on_ready():
